f8621536870214f1207804006460e361ad5b9486
RikedyP <rikedyp@gmail.com>
2022-08-08 16:38:12 +0100

    basic curl test; set entrypoint permissions

--- CI/test-endpoints.sh
+++ CI/test-endpoints.sh
@@ -7,7 +7,7 @@ touch ${TESTOUT}
 echo "Testout is : ${TESTOUT}"
 
 RES=$(curl -o /dev/null -s -w "%{http_code}\n" "${URL}/persons")
-if ! echo $RES | grep 200 > /dev/null; then
+if ! [ $RES -eq "200" ]; then
     echo "FAILED** Dyalog CMS server returned HTTP status code ${RES}" | tee -a ${TESTOUT}
     exit 1
 fi
