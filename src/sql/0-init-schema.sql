a8e8911790f7357f08df88a267cab9242ee75bb0
RikedyP <rikedyp@users.noreply.github.com>
2022-08-18 13:36:43 +0100

    fail if cannot connect to db; define migrations in SetupSchema

--- src/sql/0-init-schema.sql
+++ src/sql/0-init-schema.sql
@@ -1,11 +1,3 @@
-CREATE TABLE `migrations` (
-  PRIMARY KEY (`id`),
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  `exec_ts` datetime DEFAULT CURRENT_TIMESTAMP  
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 CREATE TABLE `dtv_videos` (
     PRIMARY KEY(`id`),
     `id` int(11) NOT NULL AUTO_INCREMENT,
