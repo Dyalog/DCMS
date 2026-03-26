341cb72117a37ee8ee1ac15aed16355f88f4443a
RikedyP <rikedyp@gmail.com>
2026-03-26 16:04:25 +0000

    remove category table #111

---
+++ sql/16-drop-category-table.sql
@@ -0,0 +1,5 @@
+-- Drop category table as duplicate of presentation_category
+-- See https://github.com/Dyalog/DCMS/issues/111
+DROP TABLE `category`;
+ALTER TABLE `presentation` ADD CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `presentation_category`(`id`)
+    ON DELETE CASCADE ON UPDATE RESTRICT;
